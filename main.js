f20b1e85a4cff6f40cdb512ec2826b0352672657
juangunilla <gunillajuan@gmail.com>
2022-09-02 19:49:10 -0300

    update css and index

--- main.js
+++ main.js
@@ -12,7 +12,6 @@
 var elms = ['track', 'timer', 'duration', 'playBtn', 'pauseBtn', 'prevBtn', 'nextBtn', 'playlistBtn', 'volumeBtn', 'progress', 'bar', 'wave', 'loading', 'playlist', 'list', 'volume', 'barEmpty', 'barFull', 'sliderBtn'];
 elms.forEach(function(elm) {
   window[elm] = document.getElementById(elm);
-  console.log(elm)
 });
 
 /**
